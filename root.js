0bc6755b1b34a8fb5d0646e66bf52f71d7db215c
oleksandr-ishchuk <alexmway@mail.ru>
2024-03-24 22:09:27 +0100

    test

---
+++ root.js
@@ -0,0 +1,8 @@
+
+function Hello() {
+    return <h1>Hello World!</h1>;
+  }
+
+  const container = document.getElementById("root1");
+  const root = ReactDOM.createRoot(container);
+  root.render(<Hello />);
