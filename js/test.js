93c734fb665a717ec5009c98625f5e30167c31e0
oleksandr-ishchuk <alexmway@mail.ru>
2024-03-28 22:04:31 +0100

    test

---
+++ js/test.js
@@ -0,0 +1,7 @@
+
+
+const bb = <h1>Тест бабел</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('pozition'));
+root.render(bb);
+
--- root.js
+++ root.js
@@ -1,8 +1,9 @@
+// import {Menu} from "./react/menu.js"
 
-function Hello() {
-    return <h1>Hello</h1>;
-  }
+// function Hello() {
+//     return <h1>Hello</h1>;
+//   }
 
-  const container = document.getElementById("root1");
+  const container = document.getElementById("menu");
   const root = ReactDOM.createRoot(container);
-  root.render(<Hello />);
+  root.render(<Menu />);
