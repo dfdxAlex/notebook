c6087f019afb3aaf07747a3229b67e646760e1c6
oleksandr-ishchuk <alexmway@mail.ru>
2024-11-08 21:32:38 +0100

    add data base

--- js/view/createLine.js
+++ js/view/createLine.js
@@ -10,23 +10,22 @@ function createLine() {
 
     for (val in mas) {
         masForTranslate.push('create-pozition-legend-label'+ val +'/Disposable/Jednorazowe/Одноразові/Одноразовые');
-        innerOut += '<fieldset class="create-pozition-fieldswt">' + 
-                       '<legend>' +
-                         '<label ' +
-                           'id="create-pozition-legend-label' + val +
-                          '">' +
-                            'Одноразовые' +
-                         '</label>' +
-                        '</legend>' +
-                       '<button ' +
-                          'class="button-dell"' +
-                          'onclick="handleDellPozition('+val+');' +
-                          'createLine();' +
-                          'myTranslate();"' +
-                          'type="button">X' + 
-                        '</button>' +
-                        '<li class="button-poz">'+mas[val]+'</li> <br>' +
-                    '</fieldset>';
+        innerOut += `<fieldset class="create-pozition-fieldswt">
+                        <legend>
+                          <label
+                            id="create-pozition-legend-label${val}">
+                             Одноразовые
+                          </label>
+                         </legend>
+                        <button
+                           class="button-dell"
+                           onclick="handleDellPozition('+val+');
+                           createLine();
+                           myTranslate();"
+                           type="button">X
+                         </button>
+                        <li class="button-poz">${mas[val]}</li><br>
+                     </fieldset>`;
     }
     innerOut += '</ul>';
     pozition.innerHTML = innerOut;
